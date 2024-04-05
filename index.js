522e22b81d083af7fcd923ad31f7c008f7110ed0
Michael Mbugua <mikembugua254@gmail.com>
2024-04-05 10:40:14 +0300

    Worked on more functions

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // code your solution here
+
+//Using Function Declaration
+function saturdayFun(message){
+    
+    const defaultMessage = "roller-skate";
+    return `This Saturday, I want to ${message || defaultMessage}!`;
+}
+
+// Using Function Expression
+const mondayWork = function(activity) {
+  const defaultActivity = "go to the office";
+  return `This Monday, I will ${activity || defaultActivity}.`;
+};
+
+function wrapAdjective(flair = "*") {
+  return function(adjective = "special") {
+      return `You are ${flair}${adjective}${flair}!`;
+  };
+}
